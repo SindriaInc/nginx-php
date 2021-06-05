e5fa5d92cf054cd287be848fa779a2cf43c9c35b
lucapitzoi <luca.pitzoi@sindria.org>
2021-06-05 18:48:43 +0200

    Init refactor

--- build.sh
+++ build.sh
@@ -16,24 +16,17 @@ if [[ -z "$3" ]]; then
     exit 1
 fi
 
-if [[ -z "$4" ]]; then
-    echo "Provide a tag env as fourth argument (local or production)"
-    exit 1
-fi
-
 
 IMAGE_NAME=$1
 TAG_VERSION=$2
 TAG_SUFFIX=$3
-TAG_ENV=$4
 
 HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 
 docker build ./src \
-    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX}-${TAG_ENV} \
+    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX} \
     --build-arg TAG_VERSION=${TAG_VERSION} \
     --build-arg TAG_SUFFIX=${TAG_SUFFIX} \
-    --build-arg TAG_ENV=${TAG_ENV} \
     --build-arg HOST_USER_UID=${HOST_USER_UID} \
     --build-arg TIMEZONE=${TIMEZONE}
