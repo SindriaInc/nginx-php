a5d53494c3422d4232726b26de963f6a1aacfff2
lucapitzoi <luca.pitzoi@sindria.org>
2021-06-09 17:47:50 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -15,11 +15,6 @@ if [[ -z "$3" ]]; then
     exit 1
 fi
 
-if [[ -z "$4" ]]; then
-    echo "Provide a tag env as fourth argument (local or production)"
-    exit 1
-fi
-
 IMAGE_NAME=$1
 TAG_VERSION=$2
 TAG_SUFFIX=$3
@@ -28,9 +23,8 @@ HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 
 docker build ./src \
-    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX}-${TAG_ENV} \
+    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX} \
     --build-arg TAG_VERSION=${TAG_VERSION} \
     --build-arg TAG_SUFFIX=${TAG_SUFFIX} \
-    --build-arg TAG_ENV=${TAG_ENV} \
     --build-arg HOST_USER_UID=${HOST_USER_UID} \
     --build-arg TIMEZONE=${TIMEZONE}
