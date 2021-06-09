07bc0ec8f54e1d14f898943237e28da2fd72ba44
lucapitzoi <luca.pitzoi@sindria.org>
2021-06-09 17:44:52 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -23,9 +23,8 @@ HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 
 docker build ./src \
-    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX}-${TAG_ENV} \
+    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX} \
     --build-arg TAG_VERSION=${TAG_VERSION} \
     --build-arg TAG_SUFFIX=${TAG_SUFFIX} \
-    --build-arg TAG_ENV=${TAG_ENV} \
     --build-arg HOST_USER_UID=${HOST_USER_UID} \
     --build-arg TIMEZONE=${TIMEZONE}
