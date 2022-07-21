11ba283c5d85523d0fc494745ede3bc2e4f3bcb1
lucapitzoi <luca.pitzoi@sindria.org>
2022-07-21 05:02:37 +0200

    Added multi arch with manifest

--- build.sh
+++ build.sh
@@ -12,7 +12,12 @@ fi
 
 
 if [[ -z "$3" ]]; then
-    echo "Provide a valid php version as third argument (5.6 7.0 7.1 7.2 7.3 7.4)"
+    echo "Provide a valid php version as third argument (7.0 7.1 7.2 7.3 7.4 8.0 8.1)"
+    exit 1
+fi
+
+if [[ -z "$4" ]]; then
+    echo "Provide a tag arch as fourth argument (eg. amd64, arm64)"
     exit 1
 fi
 
@@ -20,12 +25,14 @@ fi
 IMAGE_NAME=$1
 TAG_VERSION=$2
 TAG_SUFFIX=$3
+TAG_ARCH=$4
 
 HOST_USER_UID=1000
 TIMEZONE=Europe/Rome
 
 docker build ./src \
-    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX} \
+    --tag ${IMAGE_NAME}:${TAG_VERSION}-${TAG_SUFFIX}-${TAG_ARCH} \
+    --build-arg ARCH=${TAG_ARCH} \
     --build-arg TAG_VERSION=${TAG_VERSION} \
     --build-arg TAG_SUFFIX=${TAG_SUFFIX} \
     --build-arg HOST_USER_UID=${HOST_USER_UID} \
